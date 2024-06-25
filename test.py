b605599ba02b6b14785f21cbee937050c96f20ac
thiyagoocse <thiyagoocse@gmail.com>
2024-06-25 12:09:51 +0200

    Update test.py
    Updated the script for passing the credentials through .env file

--- test.py
+++ test.py
@@ -1,23 +1,32 @@
 from playwright.sync_api import sync_playwright, Playwright
 from playwright.sync_api import Page, expect
+from dotenv import load_dotenv
+import os
 import time
+
+load_dotenv()
+url=os.getenv("URL")
+email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+# Function for Application Login
 def login_developer_portal(playwright: Playwright):
     chromium = playwright.chromium
     browser = chromium.launch(headless=False)
     page = browser.new_page()
     # open developer portal
-    page.goto("https://developer.dnb.no/")
+    page.goto(url)
     # ignore cookies popup
     page.locator("//a[text()=\"OK\"]").click()
     # click login button
     page.locator('//a[@href="/login"]').click()
-    page.locator('//*[text()="Email address"]/parent::span//input').type("thiyagoocse@gmail.com")
-    page.locator('//*[text()="Password"]/ancestor::label/parent::div//input').type("Test@123")
+    page.locator('//*[text()="Email address"]/parent::span//input').type(email)
+    page.locator('//*[text()="Password"]/ancestor::label/parent::div//input').type(password)
     page.locator('//*[text()="Log in"]/parent::button').click()
     page.wait_for_selector('//*[text()="Log out"]/parent::button',state="visible")
-
     return browser, page
 
+# Function to Create Application & Select the Existing API
 def create_app(page: Page ):
     page.locator('//a[@href="/application/new"]').click()
     page.locator('//*[text()="Application name"]/parent::span//input').type("test-app")
@@ -33,6 +42,7 @@ def create_app(page: Page ):
     page.locator('//*[text()="Go to application"]/parent::a').click()
     return page
 
+# Function to Verify the details in the newly created APP & Delete APP
 def verify_and_delete_app(app_page: Page):
     appid = app_page.locator('//*[text()="App-Id"]/following-sibling::p').text_content()
     print(f"Appid Id is : {appid}")
@@ -42,28 +52,30 @@ def verify_and_delete_app(app_page: Page):
     app_page.locator('//*[text()="Log out"]/parent::button').click()
     print("Test 1 - CreateApp,VerifyAPP,DeleteAPP - Successful")
    
+# Function to Verify the API Explorer through BrowseAPI Option  
 def browse_app(api_explorer: Page):
     api_explorer.locator('//*[text()="Browse APIs"]/parent::a').click()
     api_explorer.wait_for_selector('//*[text()="Discover APIs that enable you to create great user experiences"]/parent::div/p',state="visible")
     
+    # Verify the list of APIs in 'ALL' Category Selection
     api_explorer.locator('//*[text()="All"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Information Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Account Pre-validation"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Currencies"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Payment Initiation Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="PSD2 Fallback"]/parent::p',state="visible")
-
+    # Verify the list of APIs in 'Corporate APIs' Category Selection
     api_explorer.locator('//*[text()="Corporate APIs"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Pre-validation"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Currencies"]/parent::p',state="visible")
-
+    # Verify the list of APIs in 'Regulatory APIs' Category Selection
     api_explorer.locator('//*[text()="Regulatory APIs"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Information Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Payment Initiation Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="PSD2 Fallback"]/parent::p',state="visible")
-    api_explorer.locator('//*[text()="Log out"]/parent::button').click()
     print("Test 2.1 - Navigation through Browse API - Successful")
 
+# Function to Verify the API Explorer through TopMenu Option  
 def topmenu_navigation(api_explorer: Page):
 
     api_explorer.locator('//*[text()="Menu"]/parent::button').click()
@@ -71,40 +83,42 @@ def topmenu_navigation(api_explorer: Page):
 
     api_explorer.wait_for_selector('//*[text()="Discover APIs that enable you to create great user experiences"]/parent::div/p',state="visible")
     
+    # Verify the list of APIs in 'ALL' Category Selection
     api_explorer.locator('//*[text()="All"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Information Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Account Pre-validation"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Currencies"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Payment Initiation Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="PSD2 Fallback"]/parent::p',state="visible")
-
+    # Verify the list of APIs in 'Corporate APIs' Category Selection
     api_explorer.locator('//*[text()="Corporate APIs"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Pre-validation"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Currencies"]/parent::p',state="visible")
-
+    # Verify the list of APIs in 'Regulatory APIs' Category Selection
     api_explorer.locator('//*[text()="Regulatory APIs"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Information Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Payment Initiation Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="PSD2 Fallback"]/parent::p',state="visible")
-    api_explorer.locator('//*[text()="Log out"]/parent::button').click()
     print("Test 2.2 - Navigation through Top Menu Option - Successful")
-    
+
+# Function to Verify the API Explorer through BottomMenu Option      
 def bottommenu_navigation(api_explorer: Page):
 
     api_explorer.locator('//*[text()="API Explorer"]//parent::a').click()
     api_explorer.wait_for_selector('//*[text()="Discover APIs that enable you to create great user experiences"]/parent::div/p',state="visible")
     
+    # Verify the list of APIs in 'ALL' Category Selection
     api_explorer.locator('//*[text()="All"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Information Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Account Pre-validation"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Currencies"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Payment Initiation Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="PSD2 Fallback"]/parent::p',state="visible")
-
+    # Verify the list of APIs in 'Corporate APIs' Category Selection
     api_explorer.locator('//*[text()="Corporate APIs"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Pre-validation"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Currencies"]/parent::p',state="visible")
-
+    # Verify the list of APIs in 'Regulatory APIs' Category Selection
     api_explorer.locator('//*[text()="Regulatory APIs"]/parent::button').click()
     api_explorer.wait_for_selector('//*[text()="Account Information Service"]/parent::p',state="visible")
     api_explorer.wait_for_selector('//*[text()="Payment Initiation Service"]/parent::p',state="visible")
@@ -112,22 +126,17 @@ def bottommenu_navigation(api_explorer: Page):
     api_explorer.locator('//*[text()="Log out"]/parent::button').click()
     print("Test 2.3 - Navigation through Bottom Menu Option - Successful")
 
-#Test1: Create APP, Verify APP, Delete APP    
+# Test Cases
+# Test1: Create APP, Verify APP, Delete APP    
 with sync_playwright() as playwright:
     browser, login_page = login_developer_portal(playwright)
     app_page = create_app(login_page)
     verify_and_delete_app(app_page)
     browser.close()
 
-#Test2: API Explorer Different Navigations
+# Test2: API Explorer Different Navigations
 with sync_playwright() as playwright:
     browser, login_page = login_developer_portal(playwright)
     api_explorer = browse_app(login_page)
-
-with sync_playwright() as playwright:
-    browser, login_page = login_developer_portal(playwright)
     api_explorer = topmenu_navigation(login_page)
-
-with sync_playwright() as playwright:
-    browser, login_page = login_developer_portal(playwright)
     api_explorer = bottommenu_navigation(login_page)
